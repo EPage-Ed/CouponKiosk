f75aab4a6cf824430e8812be171dc0389b9c1f90
Ed_A <arenberg@epage.com>
2020-05-08 21:49:02 -0700

    Initial Commit

---
+++ CouponKiosk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CouponKiosk
+//
+//  Created by Edward Arenberg on 5/8/20.
+//  Copyright © 2020 Edward Arenberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
